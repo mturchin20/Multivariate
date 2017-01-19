0b553b70f3003619c6908e53b9e6725397efe936
mturchin20 <mturchin20@gmail.com>
2017-01-19 00:39:55 -0600

    20170119 -- Updating MainScipt file and MatchSNPs script

--- DataAnalysis.MatchSNPsBy.MAF_GeneProx.vs1.py
+++ DataAnalysis.MatchSNPsBy.MAF_GeneProx.vs1.py
@@ -3,6 +3,7 @@
 import sys
 import re
 import gzip
+import os
 import random
 from argparse import ArgumentParser
 
@@ -42,6 +43,14 @@ random.seed(int(args.seed))
 #Initializing global variables
 PerPermSNPUsedRecord = []
 
+#Removing old files
+for i in range(int(args.N)):
+	outfile1fullName = args.outfile1 + "Permutation" + str(i+1) + ".txt.gz"
+	try:
+		os.remove(outfile1fullName)
+	except OSError:
+		sys.stderr.write("File not found to remove: " + outfile1fullName + "\n")
+
 if args.file1 == "-":
 	file1 = sys.stdin
 elif re.search('gz$', args.file1):
@@ -54,9 +63,9 @@ for line1 in file1:
 	
 	MatchedSNPs1 = []
 
-	MAFLowerBound = int(line1[3]) - .05
-	MAFUpperBound = int(line1[3]) + .05
-	20kbProximityFlag = int(line1[6])
+	MAFLowerBound = float(line1[3]) - .05
+	MAFUpperBound = float(line1[3]) + .05
+	ProximityFlag20kb = int(line1[6])
 	NumberWithin250kbLowerBound = int(line1[9]) - .1*int(line1[9])
 	NumberWithin250kbUpperBound = int(line1[9]) + .1*int(line1[9])
 
@@ -67,39 +76,46 @@ for line1 in file1:
 	else:
 		file2 = open(args.file2, 'r')
 
+	sys.stderr.write(",".join(line1) + "\t" + ",".join(map(str, [MAFLowerBound, MAFUpperBound, ProximityFlag20kb, NumberWithin250kbLowerBound, NumberWithin250kbUpperBound])) + "\n")
+
 	for line2 in file2:
 		line2 = line2.rstrip().split()	
 
-#		if int(line2[3]) >= MAFLowerBound and int(line2[3]) <= MAFUpperBound and int(line2[6]) == 20kbProximityFlag:
-#		if int(line2[3]) >= MAFLowerBound and int(line2[3]) <= MAFUpperBound and int(line2[6]) == 20kbProximityFlag and int(line2[9]) >= NumberWithin250kbLowerBound and int(line2[9]) <= NumberWithin250kbLowerBound:
-		if int(line2[3]) >= MAFLowerBound and int(line2[3]) <= MAFUpperBound:
-			if int(line2[6]) == 20kbProximityFlag:
-				if int(line2[9]) >= NumberWithin250kbLowerBound and int(line2[9]) <= NumberWithin250kbLowerBound:
+#		if int(line2[3]) >= MAFLowerBound and int(line2[3]) <= MAFUpperBound and int(line2[6]) == ProximityFlag20kb:
+#		if float(line2[3]) >= MAFLowerBound and float(line2[3]) <= MAFUpperBound and int(line2[6]) == ProximityFlag20kb and int(line2[9]) >= NumberWithin250kbLowerBound and int(line2[9]) <= NumberWithin250kbLowerBound:
+		if float(line2[3]) >= MAFLowerBound and float(line2[3]) <= MAFUpperBound:
+			if int(line2[6]) == ProximityFlag20kb:
+				if int(line2[9]) >= NumberWithin250kbLowerBound and int(line2[9]) <= NumberWithin250kbUpperBound:
 					MatchedSNPs1.append(line2)
 
 	file2.close()
-	
-	if len(MatchedSNPs1) <= int(args.N):
-		sys.exit('Error1a -- number of available matched SNPs is less than the desired number of permutations for the original seed SNP. Error occured on: ' + ",".join(map(str, line1)))
 
-	for i in range(args.N):
-		outfile1.fullName = args.outfile1 + "Permutation" + args.N + ".txt.gz"
-		with gzip.open(outfile1.fullName, 'ab') as outfile1:
-			if len(PerPermSNPUsedRecord) != i+1:
-				PerPermSNPUsedRecord.append({})
-			PermSNPChosen = random.choice(MatchedSNPs1)
-			WhileLoopCheck = 0
-			WhileLoopCount = 0
-			while WhileLoopCount < 100000:
-				if str(PermSNPChosen[0]) not in PerPermSNPUsedRecord[i]:
-					break
-				else:
-					PermSNPChosen = random.choice(MatchedSNPs1)
-					WhileLoopCount += 1
-			if WhileLoopCount == 100000:
-				sys.exit('Error1b -- While loop for checking whether there are unused, matched SNPs left in the matched SNP pool reached 100000 iterations, indicating an error & possible inifinite loop occuring. Error occured on: ' + ",".join(map(str, line1)))
-			outfile1.write("\t".join(map(str, PermSNPChosen)) + "\t" + str(len(MatchedSNPs1)) + "\t" + ",".join(map(str, line1)))
-			PerPermSNPUsedRecord[i][str(PermSNPChosen[0])] = 1
+	sys.stderr.write(str(len(MatchedSNPs1)) + "\n")
+
+#	if len(MatchedSNPs1) <= int(args.N):
+#		sys.exit('Error1a -- number of available matched SNPs is less than the desired number of permutations for the original seed SNP. Error occured on: ' + ",".join(map(str, line1)))
+	if len(MatchedSNPs1) <= 1000:
+		sys.stderr.write("Warning1a -- number of available matched SNPs is particularly low for a given seed SNP. Warning occured on: " + ",".join(map(str, line1)) + "," + str(len(MatchedSNPs1)) + "\n")
+
+	for i in range(int(args.N)):
+		outfile1fullName = args.outfile1 + "Permutation" + str(i+1) + ".txt.gz"
+		outfile1 = gzip.open(outfile1fullName, 'ab')
+		if len(PerPermSNPUsedRecord) != i+1:
+			PerPermSNPUsedRecord.append({})
+		PermSNPChosen = random.choice(MatchedSNPs1)
+		WhileLoopCheck = 0
+		WhileLoopCount = 0
+		while WhileLoopCount < 100000:
+			if str(PermSNPChosen[0]) not in PerPermSNPUsedRecord[i]:
+				break
+			else:
+				PermSNPChosen = random.choice(MatchedSNPs1)
+				WhileLoopCount += 1
+		if WhileLoopCount == 100000:
+			sys.exit('Error1b -- While loop for checking whether there are unused, matched SNPs left in the matched SNP pool reached 100000 iterations, indicating an error & possible inifinite loop occuring. Error occured on: ' + ",".join(map(str, line1)) + '\t' + str(len(MatchedSNPs1)))
+		outfile1.write("\t".join(map(str, PermSNPChosen)) + "\t" + str(len(MatchedSNPs1)) + "\t" + ",".join(map(str, line1)) + "\n")
+		PerPermSNPUsedRecord[i][str(PermSNPChosen[0])] = 1
+		outfile1.close()
 
 file1.close()
 
